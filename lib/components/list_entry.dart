abf9738ac5d8f6ec6ee4871b5dc32c0e1c77fe0c
Supercip971 <supercyp971@gmail.com>
2023-05-18 16:42:31 -0400

    fix: remove unused code in list_entry.dart

--- lib/components/list_entry.dart
+++ lib/components/list_entry.dart
@@ -28,24 +28,6 @@ class ListEntry extends StatefulWidget {
   State<ListEntry> createState() => _ListEntry();
 }
 
-class AnimatedCheckIcon extends AnimatedWidget {
-  const AnimatedCheckIcon({super.key, required Animation<double> animation})
-      : super(listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
-      ),
-    );
-  }
-}
-
 class _ListEntry extends State<ListEntry> with SingleTickerProviderStateMixin {
   AnimationController? _controller;
 
