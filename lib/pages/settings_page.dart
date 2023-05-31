d7c8a7fb3939f96080bb0811ecfc2fd1762d085e
Supercip971 <supercyp971@gmail.com>
2023-05-31 15:59:01 -0400

    fix: fixed y overflow in settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -101,189 +101,189 @@ class _SettingsPage extends State<SettingsPage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: pady(padx(Column(children: [
-          Text("Settings", style: Theme.of(context).textTheme.titleLarge),
-          Column(
-            children: [
-              Card(
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+        body: SingleChildScrollView(
+            child: pady(padx(Column(
+          children: [
+            Text("Settings", style: Theme.of(context).textTheme.titleLarge),
+
+            Card(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.outline,
                 ),
-                child: Column(children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTap: () => {
-                                  setState(() {
-                                    set.settings.compactMode = false;
-                                    set.syncSettings();
-                                  })
-                                },
-                            child: pad(Container(
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10.0)),
-                                  border: Border.all(
-                                    color: (!set.compactMode
-                                        ? Theme.of(context).colorScheme.primary
-                                        : Colors.transparent),
-                                    width: 2,
-                                  ),
-                                ),
-                                child: SettingsCompactView(height: 1.0)))),
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTap: () => {
-                                  setState(() {
-                                    set.settings.compactMode = true;
-                                    set.syncSettings();
-                                  })
-                                },
-                            child: pad(Container(
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10.0)),
-                                  border: Border.all(
-                                    color: (set.compactMode
-                                        ? Theme.of(context).colorScheme.primary
-                                        : Colors.transparent),
-                                    width: 2,
-                                  ),
-                                ),
-                                child: SettingsCompactView(height: 0.5)))),
-                      )
-                    ],
-                  ),
-                  // COMPACT MODE
-                  pady(
-                    Material(
-                        elevation: 0.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)),
-                        surfaceTintColor:
-                            Theme.of(context).colorScheme.surfaceTint,
-                        child: pad(
-                            Column(children: [
-                              pady(
-                                  Row(
-                                    children: [
-                                      Text("Compact mode",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleMedium),
-                                      Spacer(),
-                                      Switch(
-                                          value: set.compactMode,
-                                          onChanged: (v) => {
-                                                setState(() {
-                                                  set.settings.compactMode = v;
-
-                                                  set.syncSettings();
-                                                })
-                                              })
-                                    ],
-                                  ),
-                                  factor: 0.5),
-                              Text(
-                                  "Select it if you want to see more items on the screen.")
-                            ]),
-                            factor: 1)),
-                  ),
-                ]),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
               ),
-              //
-              // DARK MODE
-              //
-              Column(
-                children: [
-                  Card(
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Theme.of(context).colorScheme.outline,
-                      ),
-                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+              child: Column(children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                          behavior: HitTestBehavior.opaque,
+                          onTap: () => {
+                                setState(() {
+                                  set.settings.compactMode = false;
+                                  set.syncSettings();
+                                })
+                              },
+                          child: pad(Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0)),
+                                border: Border.all(
+                                  color: (!set.compactMode
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Colors.transparent),
+                                  width: 2,
+                                ),
+                              ),
+                              child: SettingsCompactView(height: 1.0)))),
                     ),
-                    child: Column(children: [
-                      pad(Row(
-                        children: [
-                          Expanded(
-                              child: IconButton(
-                                  color: set.settings.darkMode
+                    Expanded(
+                      child: GestureDetector(
+                          behavior: HitTestBehavior.opaque,
+                          onTap: () => {
+                                setState(() {
+                                  set.settings.compactMode = true;
+                                  set.syncSettings();
+                                })
+                              },
+                          child: pad(Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0)),
+                                border: Border.all(
+                                  color: (set.compactMode
                                       ? Theme.of(context).colorScheme.primary
-                                      : Theme.of(context).colorScheme.onSurface,
-                                  onPressed: () => {
-                                        setState(() {
-                                          set.settings.darkMode = true;
+                                      : Colors.transparent),
+                                  width: 2,
+                                ),
+                              ),
+                              child: SettingsCompactView(height: 0.5)))),
+                    )
+                  ],
+                ),
+                // COMPACT MODE
+                pady(
+                  Material(
+                      elevation: 0.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      surfaceTintColor:
+                          Theme.of(context).colorScheme.surfaceTint,
+                      child: pad(
+                          Column(children: [
+                            pady(
+                                Row(
+                                  children: [
+                                    Text("Compact mode",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium),
+                                    Spacer(),
+                                    Switch(
+                                        value: set.compactMode,
+                                        onChanged: (v) => {
+                                              setState(() {
+                                                set.settings.compactMode = v;
 
-                                          set.syncSettings();
-                                        })
-                                      },
-                                  icon: Icon(Icons.dark_mode, size: 64.0))),
-                          Expanded(
-                              child: IconButton(
-                                  color: !set.settings.darkMode
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Theme.of(context).colorScheme.onSurface,
-                                  onPressed: () => {
-                                        setState(() {
-                                          set.settings.darkMode = false;
-
-                                          set.syncSettings();
-                                        })
-                                      },
-                                  icon: Icon(Icons.light_mode, size: 64.0))),
-                        ],
-                      )),
-                      // COMPACT MODE
-                      pady(
-                        Material(
-                            elevation: 0.0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0)),
-                            surfaceTintColor:
-                                Theme.of(context).colorScheme.surfaceTint,
-                            child: pad(
-                                Column(children: [
-                                  pady(
-                                      Row(
-                                        children: [
-                                          Text("Dark/Light mode",
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .titleMedium),
-                                          Spacer(),
-                                          Switch(
-                                              value: set.darkMode,
-                                              onChanged: (v) => {
-                                                    setState(() {
-                                                      set.settings.darkMode = v;
-
-                                                      set.syncSettings();
-                                                    })
-                                                  })
-                                        ],
-                                      ),
-                                      factor: 0.5),
-                                  Text(
-                                      "Select if you want to change the overall theme.")
-                                ]),
-                                factor: 1)),
-                      ),
-                    ]),
+                                                set.syncSettings();
+                                              })
+                                            })
+                                  ],
+                                ),
+                                factor: 0.5),
+                            Text(
+                                "Select it if you want to see more items on the screen.")
+                          ]),
+                          factor: 1)),
+                ),
+              ]),
+            ),
+            //
+            // DARK MODE
+            //
+            Column(
+              children: [
+                Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(12)),
                   ),
-                ],
-              ),
-            ],
-          )
-        ]))));
+                  child: Column(children: [
+                    pad(Row(
+                      children: [
+                        Expanded(
+                            child: IconButton(
+                                color: set.settings.darkMode
+                                    ? Theme.of(context).colorScheme.primary
+                                    : Theme.of(context).colorScheme.onSurface,
+                                onPressed: () => {
+                                      setState(() {
+                                        set.settings.darkMode = true;
+
+                                        set.syncSettings();
+                                      })
+                                    },
+                                icon: Icon(Icons.dark_mode, size: 64.0))),
+                        Expanded(
+                            child: IconButton(
+                                color: !set.settings.darkMode
+                                    ? Theme.of(context).colorScheme.primary
+                                    : Theme.of(context).colorScheme.onSurface,
+                                onPressed: () => {
+                                      setState(() {
+                                        set.settings.darkMode = false;
+
+                                        set.syncSettings();
+                                      })
+                                    },
+                                icon: Icon(Icons.light_mode, size: 64.0))),
+                      ],
+                    )),
+                    // COMPACT MODE
+                    pady(
+                      Material(
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                          surfaceTintColor:
+                              Theme.of(context).colorScheme.surfaceTint,
+                          child: pad(
+                              Column(children: [
+                                pady(
+                                    Row(
+                                      children: [
+                                        Text("Dark/Light mode",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleMedium),
+                                        Spacer(),
+                                        Switch(
+                                            value: set.darkMode,
+                                            onChanged: (v) => {
+                                                  setState(() {
+                                                    set.settings.darkMode = v;
+
+                                                    set.syncSettings();
+                                                  })
+                                                })
+                                      ],
+                                    ),
+                                    factor: 0.5),
+                                Text(
+                                    "Select if you want to change the overall theme.")
+                              ]),
+                              factor: 1)),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ],
+        )))));
   }
 }
